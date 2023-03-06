d33f087efbf810a48e501b5f224ad36fd9329409
etienne <Utilisateur@LAPTOP-4K6IVRS5>
2023-03-06 16:43:35 +0400

    dev- Synchronisation des fichier locaux avec les fichiers distants - Téléchargement des fichiers depuis le serveur -Class FTPManager dédié au manipulation lié au FTP

---
+++ Test/Classes/FTPManager.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+
+namespace Test.Classes
+{
+    class FTPManager
+    {
+        private FtpWebRequest ftpRequest;
+        private String user;
+        private String pwd;
+        private String url;
+
+
+        public FTPManager(String user, String pwd, String url)
+        {
+            this.user = user;
+            this.pwd = pwd;
+            this.url = url;
+        }
+
+        public void OpenFtpConnection()
+        {
+            try{
+                ftpRequest = (FtpWebRequest)WebRequest.Create(url);
+                ftpRequest.Credentials = new NetworkCredential(user, pwd);
+                ftpRequest.UseBinary = true;
+                ftpRequest.Method = WebRequestMethods.Ftp.ListDirectoryDetails; //liste détaillé des répertoires 
+                FtpWebResponse response = (FtpWebResponse)ftpRequest.GetResponse();
+
+                Stream responseStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(responseStream);
+                Console.WriteLine(reader.ReadToEnd());
+
+                Console.WriteLine($"Directory List Complete, status {response.StatusDescription}");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            
+
+        }
+
+        /**
+         * établie une connexion ftp puis synchonise les fichiers du répertoire local au répertoire distant
+         */
+        public void SyncFilesFromFtp(string localFolderPath)
+        {
+            try
+            {
+                //établir une connexion FTP permanente
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(url);
+                request.Credentials = new NetworkCredential(user, pwd);
+                request.Method = WebRequestMethods.Ftp.ListDirectory;
+                FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+                //liste des fichiers distants
+                List<string> remoteFiles = new List<string>();
+                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                {
+                    string line = reader.ReadLine();
+                    while (!string.IsNullOrEmpty(line))
+                    {
+                        remoteFiles.Add(line);
+                        line = reader.ReadLine(); //lecture de la ligne suivante
+                    }
+                }
+
+                //liste des fichiers locaux
+                List<string> localFiles = Directory.GetFiles(localFolderPath).ToList();
+
+                //synchroniser les fichiers
+                foreach (string remoteFilePath in remoteFiles)
+                {
+                    string fileName = Path.GetFileName(remoteFilePath);
+                    string localFilePath = Path.Combine(localFolderPath, fileName); //path dossier local + nom fichier distant
+                    Console.WriteLine("remote filepath:" + remoteFilePath);
+                    if (localFiles.Contains(localFilePath))
+                    {
+                        //cas fichier existant, vérifier la taille 
+                        long remoteFileSize = GetFileSize(request, remoteFilePath);
+                        long localFileSize = new FileInfo(localFilePath).Length;
+                        Console.WriteLine("taille du fichier local: " + localFileSize);
+                        Console.WriteLine("taille du fichier distant: " + remoteFileSize);
+                        if (remoteFileSize != localFileSize)
+                        {
+                            //télécharger la version la plus récente du fichier
+                            Console.WriteLine("Fichier existant, dl de la version la plus récente de: " + fileName);
+                            DownloadFileFromFtp(request, remoteFilePath, localFilePath);
+                        }
+                    }
+                    else
+                    {
+                        //cas d'un ouveau fichier, le télécharger
+                        Console.WriteLine("Fichier inexistant, téléchargement de: " + fileName);
+                        DownloadFileFromFtp(request, remoteFilePath, localFilePath);
+                    }
+                }
+
+                //fermer la connexion FTP
+                response.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+           
+        }
+
+
+        /**
+           * Etabli un connexion FTP  puis télécharge plusieurs fichiers suivant la liste donnée en paramètre (
+           */
+        public void DownloadMultipleFilesFromFtp(List<string> listFilesPath, string localFolderPath)
+        {
+            //établir une connexion FTP permanente
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(url);
+            request.Credentials = new NetworkCredential(user, pwd);
+            using (FtpWebResponse response = (FtpWebResponse)request.GetResponse())
+            {
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    //parcourir la liste des fichiers à télécharger
+                    foreach (string remoteFilePath in listFilesPath)
+                    {
+                        //construire l'URL complète du fichier distant
+                        Uri fileUri = new Uri(new Uri(url), remoteFilePath);
+                        string fileUrl = fileUri.ToString();
+
+                        //créer le chemin complet du fichier local
+                        string destPath = Path.Combine(localFolderPath, Path.GetFileName(remoteFilePath));
+
+                        //télécharger le fichier distant
+                        DownloadFileFromFtp(request, fileUrl, destPath);
+                    }
+                }
+            }
+        }
+
+
+        /**
+         * Se connecte à un serveur par protocole FTP et télécharge le fichiers lié à l'url
+         */
+        public void DownloadFileFromFtp(FtpWebRequest request, string fileUrl, string destPath)
+        {
+            Console.WriteLine("format de l'uri download: " + fileUrl);
+            String path = this.url + "/" + fileUrl;
+            //créer une nouvelle requête pour télécharger le fichier spécifié
+            FtpWebRequest downloadRequest = (FtpWebRequest)WebRequest.Create(path);
+            downloadRequest.Method = WebRequestMethods.Ftp.DownloadFile;
+            downloadRequest.Credentials = request.Credentials; // réattribue les paramatres de connexion
+
+            //télécharger le fichier et l'écrire dans le fichier local
+            using (FtpWebResponse response = (FtpWebResponse)downloadRequest.GetResponse())
+            {
+                using (Stream remoteStream = response.GetResponseStream())
+                {
+                    using (FileStream localStream = new FileStream(destPath, FileMode.Create))
+                    {
+                        remoteStream.CopyTo(localStream);
+                    }
+                }
+            }
+            Console.WriteLine($"Téléchargement de {fileUrl} terminé.");
+        }
+
+        /**
+         * 
+         */
+        public long GetFileSize(FtpWebRequest request, string fileUrl)
+        {
+            
+            String path= this.url + "/" + fileUrl;
+            Console.WriteLine("file uri: " + path);
+            FtpWebRequest sizeRequest = (FtpWebRequest)WebRequest.Create(path);
+            sizeRequest.Credentials = new NetworkCredential(user, pwd);
+            sizeRequest.Method = WebRequestMethods.Ftp.GetFileSize;
+            sizeRequest.UseBinary = true;
+
+            using (FtpWebResponse response = (FtpWebResponse)sizeRequest.GetResponse())
+            {             
+                return response.ContentLength;
+            }
+        }
+
+    }
+}
+
+
--- Test/Classes/Program.cs
+++ Test/Classes/Program.cs
@@ -5,46 +5,52 @@ using System.Net;
 using Test.Classes;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 
 namespace Test
 
 {
     class Program
     {
+        public const string USER = "etienne";
+        public const string PWD = "root";
+        public const string URL = "ftp://127.0.0.1:21";
+        public const string LOCALFILEPATH = @"C:\Users\Utilisateur\Desktop\fichierLocal";
 
         static void Main(string[] args)
         {
             /*******************************************Connexion et récupération des json par FTP  ****************************/
+           
+            FTPManager ftp = new FTPManager(USER, PWD, URL);
+            ftp.SyncFilesFromFtp(LOCALFILEPATH);
+            Console.ReadLine();
 
-            // TODO: 2 solution: 1-> télécharger les fichiers json ou les lire directement sur le serveur, vérifier les données et insertion
-
-
-            /******************************************************* Lecture du fichier json  **********************************/
+         
             //chemin du fichier json
-            string jsonPath = @"C:\Users\Utilisateur\Desktop\Projet 1\fichier json\BiZiiPAD_20221129_80881.json";
+            //string jsonPath = @"C:\Users\Utilisateur\Desktop\Projet 1\fichier json\BiZiiPAD_20221129_80881.json";
             //string jsonPath = @"C:\Users\Utilisateur\Desktop\Projet 1\fichier json\BiZiiPAD_20221014_79394.json";
             //string jsonPath = @"C:\Users\Utilisateur\Desktop\Projet 1\fichier json\BiZiiPAD_20221129_80882.json";
 
             // génère un objet JsonModel depuis le fichier json
-            JsonModel jsonObj = DeserialiseJson(jsonPath);
+            //JsonModel jsonObj = DeserialiseJson(jsonPath);
 
             //Console.WriteLine(jsonObj.lignes[0].codeArticle);
             //Console.WriteLine(jsonObj.lignes[1].codeArticle);
             Console.ReadLine();
 
             // test d'affichage des lignes
-            Console.WriteLine("test d'affichage des lignes du json");
-            IList<Lignes> lesLignes = jsonObj.lignes;
-            Console.WriteLine("nb de lignes:" + lesLignes.Count);
-            foreach (Lignes l in lesLignes)
-            {
-                Console.WriteLine("num ligne:" + l.numLigne);
-                Console.WriteLine("id:" + l.idligne);
-                Console.WriteLine("identete:" + l.identete);
-                Console.WriteLine("code article: " + l.codeArticle);
-            }
+            //Console.WriteLine("test d'affichage des lignes du json");
+            //IList<Lignes> lesLignes = jsonObj.lignes;
+            //Console.WriteLine("nb de lignes:" + lesLignes.Count);
+            //foreach (Lignes l in lesLignes)
+            //{
+            //    Console.WriteLine("num ligne:" + l.numLigne);
+            //    Console.WriteLine("id:" + l.idligne);
+            //    Console.WriteLine("identete:" + l.identete);
+            //    Console.WriteLine("code article: " + l.codeArticle);
+            //}
 
-            Console.ReadLine();
+            //Console.ReadLine();
 
             /*******************************************************Objet métier: ouverture d'une bdd  **********************************/
             //base comptable 
@@ -61,12 +67,26 @@ namespace Test
             Console.WriteLine("tentative de connexion a la base SAGE...");
             if (OpenDbComptable(dbCompta, paramBaseCompta) && (OpenDbCommercial(dbCommerce, paramBaseCial, dbCompta)))
             {
-
                 Console.ReadLine();
-                Createcmd(jsonObj);
+                //Createcmd(jsonObj);
                 //Createprocesscmd(jsonObj);
+
+                /******************************************************* Lecture du fichier json  **********************************/
+
+                //parcours des fichiers jsons dans le répertoire
+                List<string> localFiles = Directory.GetFiles(LOCALFILEPATH).ToList();
+                foreach (string s in localFiles)
+                {
+                    Console.WriteLine(s);
+
+                    // génère un objet JsonModel depuis le fichier json
+                    Console.WriteLine("conversion du json en modele c#");
+                    JsonModel jsonModel = DeserialiseJson(s);
+                    Createcmd(jsonModel);
+                }
             }
 
+            Console.ReadLine();
             CloseDB(dbCommerce, dbCompta);
             Console.ReadLine();
 
@@ -139,16 +159,12 @@ namespace Test
                 Console.WriteLine("Création d'un bon de commande...");
                 Objets100cLib.IBODocumentVente3 entete = null;
                 Objets100cLib.IBODocumentVenteLigne3 lignes = null;
-                Objets100cLib.IBODocument3 info = null;
 
                 try
                 {
-                    // entete = (Objets100cLib.IBODocumentVente3)dbCommerce.FactoryDocumentVente.Create();
                     entete = dbCommerce.FactoryDocumentVente.CreateType(Objets100cLib.DocumentType.DocumentTypeVenteCommande);
-
+                    //affecte un client au bon de commande
                     entete.SetDefaultClient(dbCompta.FactoryClient.ReadNumero(jsonObject.codeClient));
-
-                    //entete.Client.CT_Num = "TEST2";
                     entete.DO_Date = StringToDate(jsonObject.dateCommande, "yyyyMMddHHmmss"); // méthode de conversion
                     entete.DO_DateLivr = StringToDate(jsonObject.dateLivraison, "yyyyMMddHHmmss");
                     Objets100cLib.IBPSoucheVente souche = (Objets100cLib.IBPSoucheVente)dbCommerce.FactorySoucheVente.Create();
@@ -156,7 +172,6 @@ namespace Test
                     entete.Souche = (Objets100cLib.IBISouche)dbCommerce.FactorySoucheVente.ReadIntitule("PDA");
                     //Affecte le prochain numéro de pièce en fonction de la souche (chrono)
                     entete.SetDefaultDO_Piece();
-
                     //entete.DO_Ref = "UUUU";
                     //entete.DO_TotalHT = jsonObject.totalHT;
                     // entete.DO_TotalTTC = jsonObject.totalTTC;
@@ -164,7 +179,6 @@ namespace Test
                     Console.WriteLine("entete du document crée!");
 
                     // lister les info libres
-                    
                     Console.WriteLine("liste des champs infos libres");
                     foreach (Objets100cLib.IBIField field in dbCommerce.FactoryDocument.InfoLibreFields)
                     {
@@ -173,22 +187,18 @@ namespace Test
                     //insertion infos libres
                     entete.InfoLibre["IDBIZIIPAD"] = jsonObject.idEntete;
                     entete.Write();
-
-                    //ajout des lignes dans le document 
+                    //ajout des lignes (articles) dans le document 
                     //parcours de la collection de ligne du document json
-                    Console.WriteLine("nb de lignes:" + lesLignes.Count);
-                    foreach (Lignes l in jsonObj.lignes)
+                    Console.WriteLine("nb de lignes:" + jsonObject.lignes.Count);
+                    foreach (Lignes l in jsonObject.lignes)
                     {
                         //ajout d'un article dans le document
                         lignes = (Objets100cLib.IBODocumentVenteLigne3)entete.FactoryDocumentLigne.Create();
                         Objets100cLib.IBOArticle3 article = dbCommerce.FactoryArticle.ReadReference(l.codeArticle);
-
                         Console.WriteLine("tentative d'insértion de :" + l.codeArticle + "qte:" + l.quantiteUc);
                         lignes.SetDefaultArticle(article, l.quantiteUc);              
                         lignes.Write();
                     }
-                    //lignes.Article.AR_Ref = "08G1DANA";
-                   // lignes.Write();
                     Console.WriteLine("articles ajoutés");
                 }
                 catch (Exception e)
@@ -235,8 +245,8 @@ namespace Test
 
                     //ajout des lignes dans le document 
                     //parcours de la collection de ligne du document json
-                    Console.WriteLine("nb de lignes:" + lesLignes.Count);
-                    foreach (Lignes l in jsonObj.lignes)
+                    Console.WriteLine("nb de lignes:" + jsonObject.lignes.Count);
+                    foreach (Lignes l in jsonObject.lignes)
                     {
                         //ajout d'un article dans le document
                        
@@ -255,32 +265,7 @@ namespace Test
                     Console.WriteLine(e);
                 }
             }
-            void Readdata(Objets100cLib.BSCIALApplication100c dbcommerce)
-            {
-                /*******************test affichage enregistrement d'article ******************************/
-                Objets100cLib.IBICollection collection = null;
-                //Objets100cLib.IBOArticle3 article = null;
-                collection = dbcommerce.FactoryArticle.List;
-                //collection = dbCommerce.FactoryDocumentLigne.List;
-
-                Console.WriteLine("nombre d'objet dans la collection:" + collection.Count);
-                foreach (Objets100cLib.IBOArticle3 unArticle in collection)
-                //foreach (Objets100cLib.IBODocumentLigne3 unArticle in collection)
-                {
-                    Console.WriteLine(unArticle.AR_CodeBarre);
-                    Console.WriteLine(unArticle.AR_Design);
-
-                    //Console.WriteLine(unArticle.DateCreation);
-                    //Console.WriteLine(unArticle.Collaborateur); 
-                }
-
-                String article = dbcommerce.FactoryArticle.ReadReference("08G1DANA").AR_CodeBarre;
-                Console.WriteLine("article: " + article);
-                Console.WriteLine("article: " + dbcommerce.FactoryArticle.ReadCodeBarre("0000030043992").AR_DateCreation);
-                Console.ReadLine();
-                dbcommerce.FactoryDocumentLigne.ReadLigne(734124);
-            }
-
+           
             /**
              * Converti un fichier json en objet C#
              */
@@ -316,60 +301,6 @@ namespace Test
                     throw new ArgumentException("La chaîne fournie n'est pas au format attendu", nameof(dateString));
                 }
             }
-
-            
-            /**
-             * Etabli un connexion FTP  puis télécharge plusieurs fichiers suivant la liste donnée en paramètre (
-             */
-            void DownloadMultipleFilesFromFtp(String user, String pwd, String url, List<string> listFilesPath, string localFolderPath)
-            {
-                //établir une connexion FTP permanente
-                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(url);
-                request.Credentials = new NetworkCredential(user, pwd);
-                FtpWebResponse response = (FtpWebResponse)request.GetResponse();
-                Stream responseStream = response.GetResponseStream();
-
-                //parcourir la liste des fichiers à télécharger
-                foreach (string remoteFilePath in listFilesPath)
-                {
-                    //construire l'URL complète du fichier distant
-                    string fileUrl = Path.Combine(url, remoteFilePath);
-
-                    //créer le chemin complet du fichier local
-                    string destPath = Path.Combine(localFolderPath, Path.GetFileName(remoteFilePath));
-
-                    //télécharger le fichier distant
-                    DownloadFileFromFtp(request, responseStream, fileUrl, destPath);
-                }
-
-                //fermer la connexion FTP
-                response.Close();
-            }
-
-            /**
-             * Se connecte à un serveur par protocole FTP et télécharge le fichiers lié à l'url
-             */
-            void DownloadFileFromFtp(FtpWebRequest request, Stream responseStream, string fileUrl, string destPath)
-            {
-                //créer une nouvelle requête pour télécharger le fichier spécifié
-                FtpWebRequest downloadRequest = (FtpWebRequest)WebRequest.Create(fileUrl);
-                downloadRequest.Method = WebRequestMethods.Ftp.DownloadFile;
-                downloadRequest.Credentials = request.Credentials; // réattribue les paramatres de connexion
-
-                //télécharger le fichier et l'écrire dans le fichier local
-                using (FtpWebResponse response = (FtpWebResponse)downloadRequest.GetResponse())
-                {
-                    using (Stream remoteStream = response.GetResponseStream())
-                    {
-                        using (FileStream localStream = new FileStream(destPath, FileMode.Create))
-                        {
-                            remoteStream.CopyTo(localStream);
-                        }
-                    }
-                }
-                Console.WriteLine($"Téléchargement de {fileUrl} terminé.");
-            }
-
         }
     }
 }
